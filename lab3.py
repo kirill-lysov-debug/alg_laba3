aa94d301b197f75258a9ee71d351269402a25b51
kirill-lysov-debug <kirill-lysov@inbox.ru>
2026-05-26 14:34:22 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,1 +1,46 @@
+class Task:
+    def __init__(self, id: int, description: str = "", priority: int = 1):
+        self.id = id
+        self.description = description
+        self.priority = priority
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, task: Task):
+        self.items.append(task)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop(0)
+
+    def front(self):
+        if self.isEmpty():
+            return None
+        return self.items[0]
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+if __name__ == "__main__":
+    queue = Queue()
+    
+    task1 = Task(1, "Оформить карту", 1)
+    queue.enqueue(task1)
+    
+    task2 = Task(2, "Оплатить задолженность", 3)
+    queue.enqueue(task2)
+    
+    print(f"Количество задач в очереди: {queue.size()}")
+    
+    print(queue.front().description)
+    
+    queue.dequeue()
+    print(queue.front().description)
+    
+    print(f"Количество задач в очереди после удаления: {queue.size()}")
